4500b97538811249c65347c75b7ab0403ce0edef
Fauzan Azima <fauzannazimaaa123@gmail.com>
2025-07-21 11:52:59 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Dashboard;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth/login');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/dashboard', Dashboard::class)->names('dashboard');
