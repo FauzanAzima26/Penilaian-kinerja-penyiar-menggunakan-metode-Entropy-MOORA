881931fa2a4d1ca96af27a96fbd6a7006480820a
Fauzan Azima <fauzannazimaaa123@gmail.com>
2025-07-21 20:41:32 +0700

    memperbaiki navbar

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -17,7 +17,7 @@
             <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                 <a href="#" class="dropdown-item">
                     <div class="d-flex align-items-center">
-                        <img class="rounded-circle" src="img/user.jpg" alt=""
+                        <img class="rounded-circle" src="{{ asset('assets') }}/img/11539820.png" alt=""
                             style="width: 40px; height: 40px;">
                         <div class="ms-2">
                             <h6 class="fw-normal mb-0">Jhon send you a message</h6>
@@ -28,7 +28,7 @@
                 <hr class="dropdown-divider">
                 <a href="#" class="dropdown-item">
                     <div class="d-flex align-items-center">
-                        <img class="rounded-circle" src="img/user.jpg" alt=""
+                        <img class="rounded-circle" src="{{ asset('assets') }}/img/11539820.png" alt=""
                             style="width: 40px; height: 40px;">
                         <div class="ms-2">
                             <h6 class="fw-normal mb-0">Jhon send you a message</h6>
@@ -77,9 +77,9 @@
         </div>
         <div class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                <img class="rounded-circle me-lg-2" src="img/user.jpg" alt=""
+                <img class="rounded-circle me-lg-2" src="{{ asset('assets') }}/img/11539820.png" alt=""
                     style="width: 40px; height: 40px;">
-                <span class="d-none d-lg-inline-flex">John Doe</span>
+                <span class="d-none d-lg-inline-flex">{{ Auth::user()->name }}</span>
             </a>
             <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                 <a href="#" class="dropdown-item">My Profile</a>
